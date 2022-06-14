bec2561f0a45a2d00f8ff4bbd9ee53b576743d6e
Nhan Cao <nhancv92@gmail.com>
2022-06-14 21:57:11 +0700

    feat: update

--- pancake-swap-interface-v1/src/hooks/useGetPriceData.ts
+++ pancake-swap-interface-v1/src/hooks/useGetPriceData.ts
@@ -12,7 +12,7 @@ type ApiResponse = {
   }
 }
 
-const api = 'https://api.pancakeswap.info/api/tokens'
+const api = 'https://api.pancakeswap.info/api/v2/tokens'
 
 const useGetPriceData = () => {
   const [data, setData] = useState<ApiResponse | null>(null)
